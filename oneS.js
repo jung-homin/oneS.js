f01f7290beac8642a1c1dca790629662554bdb4c
JUNGHOMIN <homin@JUNGHOMINs-MacBook-Air.local>
2022-08-21 23:40:55 +0900

    셀렉터 방식에서 메소드 방식으로 변경, Media 생성자 추가

--- oneS.js
+++ oneS.js
@@ -1,5 +1,5 @@
 (function (factory) {
-  $.extend($.fn, factory());
+  window.oneS = factory();
 })(function () {
   function returnValueFunc(value) {
     return function () {
@@ -7,40 +7,97 @@
     };
   }
 
-  function addEventOn(obj) {
+  function forKeys(obj, iterator) {
+    var keys = Object.keys(obj);
+    keys.forEach(function (key) {
+      iterator(key);
+    });
+  }
+
+  function forLength(obj, iterator) {
+    for (var i = 0; i < obj.length; i++) {
+      iterator(i);
+    }
+  }
+
+  function mergeObj() {
+    var resultObj = {};
+    var arg = arguments;
+    forLength(arg, function (i) {
+      var mergedObj = arg[i];
+      forKeys(mergedObj, function (key) {
+        resultObj[key] = mergedObj[key];
+      });
+    });
+    return resultObj;
+  }
+
+  function hasClass(target, htmlClass) {
+    var classList = Object.values(target.classList);
+    return classList.indexOf(htmlClass) > -1;
+  }
+
+  function addClass(target, htmlClass) {
+    target.classList.add(htmlClass);
+  }
+
+  function removeClass(target, htmlClass) {
+    target.classList.remove(htmlClass);
+  }
+
+  function addEventOn(targetQuery, obj = {}) {
     var option = {
-      target: $(this),
+      target: document.querySelector(targetQuery),
       event: "click",
       onIf: function () {
-        return !this.target.hasClass("on");
+        return !hasClass(this.target, "on");
       },
       offIf: function () {
-        return this.target.hasClass("on");
+        return hasClass(this.target, "on");
       },
       onEvent: returnValueFunc(false),
       offEvent: returnValueFunc(false),
     };
 
-    $.extend(option, obj);
+    option = mergeObj(option, obj);
 
-    $(this).on(option.event, function (e) {
+    option.target.addEventListener(option.event, function (e) {
+      console.log(option.onIf(e));
       if (option.onIf(e)) {
-        option.target.addClass("on");
+        addClass(option.target, "on");
         option.onEvent(e);
       } else if (option.offIf(e)) {
-        option.target.removeClass("on");
+        removeClass(option.target, "on");
         option.offEvent(e);
       }
     });
   }
 
-  function replaceTag() {
-    return false;
+  function Media(
+    option = {
+      mobile: 640,
+      tablet: 1220,
+      desktop: Infinity,
+    }
+  ) {
+    this.mediaQuery = option;
+    this.isMedia = function (mediaQuery) {
+      return this.currentMedia() === mediaQuery;
+    };
+    this.currentMedia = function () {
+      var mediaQuery = this.mediaQuery;
+      var resultArr = [];
+      forKeys(mediaQuery, function (key) {
+        if (window.innerWidth < mediaQuery[key]) resultArr.push(key);
+      });
+      return resultArr[0];
+    };
   }
 
   var allExports = {
     addEventOn: addEventOn,
     returnValueFunc: returnValueFunc,
+    Media: Media,
   };
 
   return allExports;
